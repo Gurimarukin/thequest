c560849472997802ef2431e0ab937ddb3115d108
Gurimarukin <naxel@laposte.net>
2023-04-03 19:28:27 +0200

    snap left

--- src/client/components/Tooltip.tsx
+++ src/client/components/Tooltip.tsx
@@ -30,13 +30,6 @@ export const Tooltip: React.FC<Props> = ({ tooltip, childrenClassName, className
         <span className="-ml-1 flex h-1 w-1 border-r-[6px] border-t-[6px] border-r-mastery4-brown-secondary border-t-transparent" />
         <span className="flex h-1 w-1 border-l-[6px] border-t-[6px] border-l-mastery4-brown-secondary border-t-transparent" />
         <div
-          // ref={elt => {
-          //   if (elt === null) return
-          //   console.log('elt.getBoundingClientRect() =', elt.getBoundingClientRect())
-          //   // console.log('elt =', elt)
-          //   // console.log('getWindowTop(elt) =', getWindowTop(elt))
-          //   // console.log('getWindowLeft(elt) =', getWindowLeft(elt))
-          // }}
           className="relative top-[6px] whitespace-nowrap border border-mastery4-brown-secondary bg-zinc-900 py-1 px-2 text-xs text-wheat"
           style={style}
         >
@@ -54,35 +47,9 @@ const getStyles = (elt: HTMLElement): React.CSSProperties => {
   const eltRect = elt.getBoundingClientRect()
   const docRect = window.document.documentElement.getBoundingClientRect()
 
-  // const halfWidth = Math.round(eltRect.width / 2)
-  // const diff = Math.round(documentRect.width - tooltipMargin - eltRect.right - halfWidth)
-  // console.log('diff =', diff)
-  // console.log('left =', diff < 0 ? diff + halfWidth : '-50%')
-  // return { left: diff < 0 ? diff + halfWidth : '-50%' }
-
   const halfWidth = Math.round(eltRect.width / 2)
-  const diff = docRect.width - tooltipMargin - (eltRect.left + halfWidth)
-  return { left: -halfWidth + Math.min(diff, 0) }
+  const diffRight = docRect.width - tooltipMargin - (eltRect.left + halfWidth)
+  const diffLeft = eltRect.left - halfWidth
+
+  return { left: -Math.min(diffLeft, 0) - halfWidth + Math.min(diffRight, 0) }
 }
-
-// type NumberKey = Extract<keyof HTMLElement, 'offsetTop' | 'offsetLeft'>
-// type Direction  ='top' | 'left'
-
-// const getWindowsPos = (direction: Direction) => (elt: HTMLElement) => getWindowPosRec(direction, elt, 0)
-
-// const getWindowPosRec = (direction: Direction, elt: Element | null, isFirst: boolean, acc: number): number => {
-//   if (elt === null || !(elt instanceof HTMLElement)) return acc
-//   return getWindowPosRec(key, elt.offsetParent, acc + elt[key])
-// }
-
-// const incr = (direction: Direction, elt: HTMLElement, isFirst: boolean) : number => {
-//   switch (direction) {
-//     case 'top':
-//       return isFirst  ?   elt.offsetTop: elt.offsetTop + elt.offsetHeight
-//     case 'left':
-//     return elt.
-//   }
-// }
-
-// const getWindowTop = getWindowsPos('offsetTop')
-// const getWindowLeft = getWindowsPos('offsetLeft')
