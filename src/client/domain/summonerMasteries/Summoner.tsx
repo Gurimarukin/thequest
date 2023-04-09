1b6815f82e82b6bd62edb5fa7f23dc80ad221b75
Gurimarukin <naxel@laposte.net>
2023-04-09 18:55:21 +0200

    summoner use Tooltip

--- src/client/domain/summonerMasteries/Summoner.tsx
+++ src/client/domain/summonerMasteries/Summoner.tsx
@@ -1,10 +1,11 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useRef } from 'react'
 
 import type { ChampionLevelOrZero } from '../../../shared/models/api/ChampionLevel'
 import type { SummonerView } from '../../../shared/models/api/summoner/SummonerView'
 import type { Dict } from '../../../shared/utils/fp'
 
 import { MasteryImg } from '../../components/MasteryImg'
+import { Tooltip } from '../../components/tooltip/Tooltip'
 import { useStaticData } from '../../contexts/StaticDataContext'
 import { InformationCircleOutline } from '../../imgs/svgIcons'
 import { NumberUtils } from '../../utils/NumberUtils'
@@ -34,6 +35,9 @@ export const Summoner = ({
 }: Props): JSX.Element => {
   const staticData = useStaticData()
 
+  const masteriesRef = useRef<HTMLDivElement>(null)
+  const infoRef = useRef<HTMLSpanElement>(null)
+
   const MasteryImgWithCount = useMemo(
     () => getMasteryImgWithCount(masteriesCount),
     [masteriesCount],
@@ -53,46 +57,54 @@ export const Summoner = ({
         </div>
       </div>
       <div className="flex flex-col items-center gap-3">
-        <div className="peer/masteries flex items-end gap-2">
-          <MasteryImgWithCount level={7} imgClassName="h-16 mt-[-6px]" />
-          <MasteryImgWithCount level={6} imgClassName="h-16 mt-[-7px] mb-[-4px]" />
-          <MasteryImgWithCount level={5} imgClassName="h-16 mt-[-11px] mb-[-6px]" />
+        <div ref={masteriesRef} className="flex items-end gap-2">
+          <MasteryImgWithCount level={7} imgClassName="w-[72px] mt-[-6px]" />
+          <MasteryImgWithCount level={6} imgClassName="w-[72px] mt-[-7px] mb-[-4px]" />
+          <MasteryImgWithCount level={5} imgClassName="w-[72px] mt-[-11px] mb-[-6px]" />
         </div>
-        <span className="flex items-center gap-2">
-          <span className="text-sm">Progression : {round(questPercents, 2)} %</span>
-          <InformationCircleOutline className="peer/info h-6 fill-current" />
-          <ul className="absolute right-0 top-[calc(100%_+_.75rem)] z-10 hidden list-disc border-2 border-mastery4-brown-secondary bg-black py-3 pr-3 pl-6 text-xs leading-6 peer-hover/info:block">
-            <li>
-              De la maîtrise 0 à la maîtrise 5, les pourcents correspondent aux points de maîtrise.
-            </li>
-            <li>Maîtrise 5 = 50 %</li>
-            <li>Chaque fragment = 3 %</li>
-            <li>
-              Chaque jeton pour la maîtrise 6 = 7 % (maîtrise 5 + 1 jeton = 57 % ; maîtrise 5 + 2
-              jetons = 64 %)
-            </li>
-            <li>Maîtrise 6 = 67 %</li>
-            <li>
-              Chaque jeton pour la maîtrise 7 = 10 % (maîtrise 6 + 1 jeton = 77 % ; maîtrise 6 + 2
-              jetons = 87 % ; maîtrise 6 + 3 jetons = 97 %)
-            </li>
-            <li>Maîtrise 7 = 100 %</li>
-          </ul>
-        </span>
-        <div className="absolute right-0 top-[calc(100%_+_.75rem)] z-10 hidden flex-col items-center gap-2 border-2 border-mastery4-brown-secondary bg-black px-5 pt-3 pb-4 peer-hover/masteries:flex">
-          <div className="flex items-end gap-1">
-            <MasteryImgWithCount level={4} imgClassName="h-12 mt-[-6px]" />
-            <MasteryImgWithCount level={3} imgClassName="h-12 mt-[-9px] mb-[-3px]" />
-            <MasteryImgWithCount level={2} imgClassName="h-12 mt-[-10px] mb-[-5px]" />
-            <MasteryImgWithCount level={1} imgClassName="h-12 mt-[-10px] mb-[-8px]" />
+        <Tooltip
+          hoverRef={masteriesRef}
+          className="grid grid-cols-[1fr] justify-items-center gap-2 px-5 pt-3 pb-4"
+        >
+          <div className="grid grid-cols-[repeat(4,54px)_34px] items-end gap-1">
+            <MasteryImgWithCount level={4} imgClassName="mt-[-6px]" />
+            <MasteryImgWithCount level={3} imgClassName="mt-[-9px] mb-[-3px]" />
+            <MasteryImgWithCount level={2} imgClassName="mt-[-10px] mb-[-5px]" />
+            <MasteryImgWithCount level={1} imgClassName="mt-[-10px] mb-[-8px]" />
             <MasteryImgWithCount
               level={0}
-              imgClassName="h-12 mt-[-10px] mb-[-8px]"
-              className="mx-[-10px]"
+              imgClassName="mt-[-10px] mb-[-8px]"
+              className="relative left-[-10px] w-[54px]"
             />
           </div>
           <span className="text-sm">Niveau de maîtrise : {totalMasteryLevel}</span>
-        </div>
+        </Tooltip>
+        <span className="flex items-center gap-2">
+          <span className="text-sm">Progression : {round(questPercents, 2)} %</span>
+          <span ref={infoRef}>
+            <InformationCircleOutline className="h-6 fill-current" />
+          </span>
+          <Tooltip hoverRef={infoRef}>
+            <ul className="list-disc pl-3 leading-6">
+              <li>
+                De la maîtrise 0 à la maîtrise 5, les pourcents correspondent aux points de
+                maîtrise.
+              </li>
+              <li>Maîtrise 5 = 50 %</li>
+              <li>Chaque fragment = 3 %</li>
+              <li>
+                Chaque jeton pour la maîtrise 6 = 7 % (maîtrise 5 + 1 jeton = 57 % ; maîtrise 5 + 2
+                jetons = 64 %)
+              </li>
+              <li>Maîtrise 6 = 67 %</li>
+              <li>
+                Chaque jeton pour la maîtrise 7 = 10 % (maîtrise 6 + 1 jeton = 77 % ; maîtrise 6 + 2
+                jetons = 87 % ; maîtrise 6 + 3 jetons = 97 %)
+              </li>
+              <li>Maîtrise 7 = 100 %</li>
+            </ul>
+          </Tooltip>
+        </span>
       </div>
     </div>
   )
@@ -110,6 +122,6 @@ const getMasteryImgWithCount =
     (
       <div className={cssClasses('flex flex-col items-center', className)}>
         <span className="text-xs">{masteriesCount[level]}</span>
-        <MasteryImg level={level} className={imgClassName} />
+        <MasteryImg level={level} className={cssClasses('w-full', imgClassName)} />
       </div>
     )
