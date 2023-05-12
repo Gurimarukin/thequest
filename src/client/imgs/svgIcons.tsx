adac185b6a40541c4143ae881145ef25a7dbef9e
Gurimarukin <naxel@laposte.net>
2023-05-12 20:39:59 +0200

    better colors

--- src/client/imgs/svgIcons.tsx
+++ src/client/imgs/svgIcons.tsx
@@ -282,6 +282,9 @@ export const TimeOutline: SVGIcon = ({ className }) => (
 
 export const ToggleFilled: SVGIcon = ({ className }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={className}>
-    <path d="M368 112H144C64.6 112 0 176.6 0 256s64.6 144 144 144h224c79.4 0 144-64.6 144-144s-64.6-144-144-144zm0 256a112 112 0 11112-112 112.12 112.12 0 01-112 112z" />
+    <path
+      d="M368 112H144C64.6 112 0 176.6 0 256s64.6 144 144 144h224c79.4 0 144-64.6 144-144s-64.6-144-144-144zm0 256a112 112 0 11112-112 112.12 112.12 0 01-112 112z"
+      fill="currentColor"
+    />
   </svg>
 )
