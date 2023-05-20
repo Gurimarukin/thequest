807eaed52f4e5aa6dd40e74e62050c79458ad2e8
Gurimarukin <naxel@laposte.net>
2023-05-20 17:55:38 +0200

    gameId isn't needed

--- src/shared/models/api/currentGame/CurrentGameInfoView.ts
+++ src/shared/models/api/currentGame/CurrentGameInfoView.ts
@@ -4,14 +4,12 @@ import { List } from '../../../utils/fp'
 import { DayJsFromISOString } from '../../../utils/ioTsUtils'
 import { ChampionKey } from '../champion/ChampionKey'
 import { CurrentGameParticipantView } from './CurrentGameParticipantView'
-import { GameId } from './GameId'
 import { GameQueue } from './GameQueue'
 import { TeamId } from './TeamId'
 
 type CurrentGameInfoView = C.TypeOf<typeof codec>
 
 const codec = C.struct({
-  gameId: GameId.codec,
   gameStartTime: DayJsFromISOString.codec,
   gameQueueConfigId: GameQueue.codec,
   bannedChampions: List.codec(
