f20b1c03f786076dc2c12357d40e5a6b15d51097
Gurimarukin <naxel@laposte.net>
2023-05-20 17:59:38 +0200

    unused label

--- src/server/models/riot/MapId.ts
+++ src/server/models/riot/MapId.ts
@@ -1,5 +1,4 @@
 import { createEnum } from '../../../shared/utils/createEnum'
-import type { Dict } from '../../../shared/utils/fp'
 
 type MapId = typeof MapId.T
 
@@ -21,22 +20,4 @@ const MapId = createEnum(
   22, // Convergence — Teamfight Tactics map
 )
 
-const label: Dict<`${MapId}`, string> = {
-  1: 'Faille de l’invocateur', // Summoner's Rift
-  2: 'Faille de l’invocateur', // Summoner's Rift
-  3: 'The Proving Grounds', // The Proving Grounds
-  4: 'Forêt torturée', // Twisted Treeline
-  8: 'La Brêche de Cristal', // The Crystal Scar
-  10: 'Forêt torturée', // Twisted Treeline
-  11: 'Faille de l’invocateur', // Summoner's Rift
-  12: 'Abîme Hurlant', // Howling Abyss
-  14: 'Le Pont du Boucher', // Butcher's Bridge
-  16: 'Cosmic Ruins', // Cosmic Ruins
-  18: 'Valoran City Park', // Valoran City Park
-  19: 'Substructure 43', // Substructure 43
-  20: 'Crash Site', // Crash Site
-  21: 'Nexus Blitz', // Nexus Blitz
-  22: 'Convergence', // Convergence
-}
-
-export { MapId, label }
+export { MapId }
