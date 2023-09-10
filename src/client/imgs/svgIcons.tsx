0f7451172c02ebef47d71483a4ab66a97e6ca8c9
Gurimarukin <naxel@laposte.net>
2023-09-10 17:15:27 +0200

    add refresh game button

--- src/client/imgs/svgIcons.tsx
+++ src/client/imgs/svgIcons.tsx
@@ -258,6 +258,27 @@ export const PersonFilled: SVGIcon = ({ className }) => (
   </svg>
 )
 
+export const RefreshOutline: SVGIcon = ({ className }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={className}>
+    <path
+      d="M320 146s24.36-12-64-12a160 160 0 10160 160"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="32"
+    />
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="32"
+      d="M256 58l80 80-80 80"
+    />
+  </svg>
+)
+
 export const RemoveOutline: SVGIcon = ({ className }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={className}>
     <path
