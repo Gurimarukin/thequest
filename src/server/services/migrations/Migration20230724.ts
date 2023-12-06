75b56991b239d261043f57b7e3f445e57afabf90
Gurimarukin <naxel@laposte.net>
2023-12-06 19:39:10 +0100

    dropCollection

--- src/server/services/migrations/Migration20230724.ts
+++ src/server/services/migrations/Migration20230724.ts
@@ -1,22 +1,16 @@
 import { pipe } from 'fp-ts/function'
-import { MongoServerError } from 'mongodb'
 
 import { DayJs } from '../../../shared/models/DayJs'
-import type { Dict } from '../../../shared/utils/fp'
 import { Future, toNotUsed } from '../../../shared/utils/fp'
 
 import type { Migration } from '../../models/migration/Migration'
 import type { MongoCollectionGetter } from '../../models/mongo/MongoCollection'
+import { MigrationUtils } from './MigrationUtils'
 
 export const Migration20230724 = (mongoCollection: MongoCollectionGetter): Migration => ({
   createdAt: DayJs.of('2023-07-24T12:09:10Z'),
   migrate: pipe(
-    mongoCollection<Dict<string, never>>('leagueEntry').future(coll => coll.drop()),
-    Future.orElse(e =>
-      e instanceof MongoServerError && e.message === 'ns not found'
-        ? Future.successful(true)
-        : Future.failed(e),
-    ),
+    MigrationUtils.dropCollection(mongoCollection, 'leagueEntry'),
     Future.map(toNotUsed),
   ),
 })
