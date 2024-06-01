517c52439d8b40edad5d901a4e4dd0cca8bb016b
Gurimarukin <naxel@laposte.net>
2024-06-01 13:43:54 +0200

    remove mastery- colors

--- src/client/utils/colors.ts
+++ src/client/utils/colors.ts
@@ -1,38 +1,38 @@
 const masteryBgColorClassName = {
-  10: 'text-[#B71D3C]',
+  10: 'text-[#b71d3c]',
   9: 'text-[#d2651b]',
   8: 'text-[#8d12c9]',
   7: 'text-[#0369a1]',
   6: 'text-[#329055]',
   5: 'text-[#94a3b8]',
-  4: 'text-mastery-3',
-  3: 'text-mastery-3',
-  2: 'text-mastery-3',
-  1: 'text-mastery-3',
+  4: 'text-[#71717a]',
+  3: 'text-[#71717a]',
+  2: 'text-[#71717a]',
+  1: 'text-[#71717a]',
 } as Record<number, string>
 
 const masteryBgGradientClassName = {
-  10: 'bg-gradient-to-r from-[#B71D3C] to-[#640411]',
-  9: 'bg-gradient-to-r from-[#d2651b] to-[#8E3D25]',
+  10: 'bg-gradient-to-r from-[#b71d3c] to-[#640411]',
+  9: 'bg-gradient-to-r from-[#d2651b] to-[#8e3d25]',
   8: 'bg-gradient-to-r from-[#8d12c9] to-[#560292]',
-  7: 'bg-gradient-to-r from-[#0369a1] to-[#324BD0]',
+  7: 'bg-gradient-to-r from-[#0369a1] to-[#324bd0]',
   6: 'bg-gradient-to-r from-[#329055] to-[#266846]',
   5: 'bg-gradient-to-r from-[#94a3b8] to-[#475569]',
-  4: 'bg-gradient-to-r from-mastery-3 to-mastery-3-bis',
-  3: 'bg-gradient-to-r from-mastery-3 to-mastery-3-bis',
-  2: 'bg-gradient-to-r from-mastery-3 to-mastery-3-bis',
-  1: 'bg-gradient-to-r from-mastery-3 to-mastery-3-bis',
+  4: 'bg-gradient-to-r from-[#71717a] to-[#52525b]',
+  3: 'bg-gradient-to-r from-[#71717a] to-[#52525b]',
+  2: 'bg-gradient-to-r from-[#71717a] to-[#52525b]',
+  1: 'bg-gradient-to-r from-[#71717a] to-[#52525b]',
 } as Record<number, string>
 
 const masteryTextColorClassName = {
-  10: 'text-[#F5378E]',
-  9: 'text-[#F6A738]',
-  8: 'text-[#E752FB]',
+  10: 'text-[#f5378e]',
+  9: 'text-[#f6a738]',
+  8: 'text-[#e752fb]',
   7: 'text-[#3b82f6]',
   6: 'text-[#22c55e]',
   5: 'text-[#cbd5e1]',
 } as Record<number, string>
-const masteryTextColorDefaultClassName = 'text-mastery-3-text'
+const masteryTextColorDefaultClassName = 'text-[#d4d4d4]'
 
 const masteryRulerColorClassName = {
   8: 'border-grey-500',
