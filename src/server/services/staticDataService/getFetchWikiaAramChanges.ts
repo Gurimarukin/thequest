ebb463b5fd6a82147fc535a9a99a2e277c11006f
Gurimarukin <naxel@laposte.net>
2024-11-19 23:01:25 +0100

    migrate getFetchWikiaAramChanges

--- src/server/services/staticDataService/getFetchWikiaAramChanges.ts
+++ src/server/services/staticDataService/getFetchWikiaAramChanges.ts
@@ -1,27 +1,39 @@
 import { apply, readonlyMap, string } from 'fp-ts'
-import { flow, pipe } from 'fp-ts/function'
+import { flow, identity, pipe } from 'fp-ts/function'
 import type { Decoder } from 'io-ts/Decoder'
 import * as D from 'io-ts/Decoder'
 import xml2js from 'xml2js'
 
+import { ValidatedNea } from '../../../shared/models/ValidatedNea'
 import type { ChampionSpellHtml } from '../../../shared/models/api/AramData'
 import { SpellName } from '../../../shared/models/api/SpellName'
 import { ListUtils } from '../../../shared/utils/ListUtils'
 import { StringUtils } from '../../../shared/utils/StringUtils'
 import type { Tuple3 } from '../../../shared/utils/fp'
-import { Either, Future, List, Maybe, NonEmptyArray, PartialDict } from '../../../shared/utils/fp'
-import { decodeError } from '../../../shared/utils/ioTsUtils'
+import {
+  Either,
+  Future,
+  IO,
+  List,
+  Maybe,
+  NonEmptyArray,
+  PartialDict,
+  Try,
+  Tuple,
+} from '../../../shared/utils/fp'
+import { decodeError, decodeErrorString } from '../../../shared/utils/ioTsUtils'
 
-import { constants } from '../../config/constants'
 import { DomHandler } from '../../helpers/DomHandler'
 import type { HttpClient } from '../../helpers/HttpClient'
 import { ChampionEnglishName } from '../../models/wikia/ChampionEnglishName'
 import type { WikiaAramChanges } from '../../models/wikia/WikiaAramChanges'
 
-const apiPhpUrl = `${constants.lolWikiaDomain}/api.php`
+const lolWikiDomain = 'https://wiki.leagueoflegends.com'
 
-export const getFetchWikiaAramChanges = (httpClient: HttpClient): Future<WikiaAramChanges> => {
-  return pipe(
+const apiPhpUrl = `${lolWikiDomain}/api.php`
+
+export function getFetchWikiaAramChanges(httpClient: HttpClient): Future<WikiaAramChanges> {
+  const fetchMapChanges: Future<string> = pipe(
     httpClient.json(
       [apiPhpUrl, 'get'],
       {
@@ -42,136 +54,251 @@ export const getFetchWikiaAramChanges = (httpClient: HttpClient): Future<WikiaAr
       ),
     ),
     Future.chain(a => pipe(a.root.ignore[0], parseXML(includeOnlyXMLDecoder, 'IncludeOnlyXML'))),
-    Future.chain(a =>
-      pipe(
-        a.includeonly,
-        string.split('\n'),
-        NonEmptyArray.init,
-        List.mkString('\n'),
-        string.split('\n|'),
-        NonEmptyArray.tail,
-        List.filterMap(
-          flow(StringUtils.matcher3(nameSpellValueRegex), Maybe.map(parseChampionChanges)),
-        ),
-        List.sequence(Future.ApplicativePar),
-      ),
-    ),
-    Future.map(
-      flow(
-        ListUtils.groupByAsMap(ChampionEnglishName.Eq)(c => [c.englishName, c]),
-        readonlyMap.map(
-          flow(
-            List.groupBy(c => c.spell),
-            PartialDict.map(
-              (nea): ChampionSpellHtml => ({
-                spell: NonEmptyArray.head(nea).html.spell,
-                description: pipe(
-                  nea,
-                  NonEmptyArray.map(c => c.html.description),
-                  List.mkString(''),
-                ),
-              }),
-            ),
-          ),
-        ),
-      ),
-    ),
+    Future.map(a => a.includeonly),
   )
 
-  function parseChampionChanges(
-    tuple: Tuple3<string, string, string>,
-  ): Future<ChampionSpellHtmlDescription> {
-    return pipe(
-      tupleChampionEnglishNameSpellNameStringDecoder.decode(tuple),
-      Either.mapLeft(decodeError('Tuple<ChampionEnglishName, SpellName, string>')(tuple)),
-      Future.fromEither,
-      Future.chain(([englishName, spell, value]) =>
-        pipe(
-          apply.sequenceS(Future.ApplyPar)({
-            spell: parseSpellWikiText(englishName, spell, `{{ai|${spell}|${englishName}}}`),
-            description: parseSpellWikiText(englishName, spell, value),
-          }),
-          Future.map((html): ChampionSpellHtmlDescription => ({ englishName, spell, html })),
-        ),
-      ),
-    )
-  }
+  return pipe(
+    fetchMapChanges,
+    Future.chainEitherK(parseRawChanges),
+    Future.map(flow(groupChangesByChampionsAndSpells, makeTemplate)),
+    Future.chain(fetchParseWikiText),
+    Future.chainIOEitherK(parseWikiHtml),
+  )
 
-  function parseSpellWikiText(
-    englishName: ChampionEnglishName,
-    spell: SpellName,
-    value: string,
-  ): Future<string> {
+  function fetchParseWikiText(text: string): Future<string> {
     return pipe(
       httpClient.json(
-        [apiPhpUrl, 'get'],
+        [apiPhpUrl, 'get'], // TODO: POST?
         {
           searchParams: {
             action: 'parse',
             format: 'json',
             contentmodel: 'wikitext',
-            text: value,
+            text,
           },
         },
         [parseTextDecoder, 'ParseText'],
       ),
-      Future.chainEitherK(res => DomHandler.of()(res.parse.text['*'])),
-      Future.chainEitherK(domHandler =>
-        pipe(
-          domHandler.window.document.body,
-          DomHandler.querySelectorEnsureOne('div.mw-parser-output > *:first-child'),
-          Either.bimap(
-            message => Error(`${englishName} ${spell}:\n${message}\nValue:\n${value}`),
-            firstChild => {
-              /* eslint-disable functional/no-expression-statements */
-              firstChild.querySelectorAll('script').forEach(e => e.remove())
-
-              firstChild.querySelectorAll('img').forEach(e =>
-                pipe(
-                  e.src,
-                  StringUtils.matcher1(imageRegex),
-                  Maybe.fold(
-                    () => undefined,
-                    newStr => e.setAttribute('src', newStr),
-                  ),
-                ),
-              )
-
-              firstChild.querySelectorAll('a').forEach(e => {
-                e.setAttribute('href', new URL(e.href, constants.lolWikiaDomain).toString())
-                e.setAttribute('target', '_blank')
-                e.setAttribute('rel', 'noreferrer')
-              })
-
-              firstChild.querySelectorAll('.inline-image').forEach(e => e.removeAttribute('style'))
-              /* eslint-enable functional/no-expression-statements */
-
-              return firstChild
-            },
-          ),
-        ),
-      ),
-      Future.map(firstChild => firstChild.outerHTML),
+      Future.map(res => res.parse.text['*']),
     )
   }
 }
 
-type ChampionSpellHtmlDescription = {
-  englishName: ChampionEnglishName
-  spell: SpellName
-  html: ChampionSpellHtml
+function groupChangesByChampionsAndSpells(
+  tuples: List<Tuple3<ChampionEnglishName, SpellName, string>>,
+): ReadonlyMap<ChampionEnglishName, PartialDict<SpellName, string>> {
+  return pipe(
+    tuples,
+    ListUtils.groupByAsMap(ChampionEnglishName.Eq)(([englishName, spell, changes]) =>
+      Tuple.of(englishName, Tuple.of(spell, changes)),
+    ),
+    readonlyMap.map(
+      flow(
+        List.groupBy(([spell]) => spell),
+        PartialDict.map(
+          flow(
+            List.map(([, changes]) => changes.trim()),
+            List.mkString('\n'),
+          ),
+        ),
+      ),
+    ),
+  )
 }
 
-const parseXML =
-  <A>(decoder: Decoder<unknown, A>, decoderName: string, options?: xml2js.ParserOptions) =>
-  (xml: string): Future<A> =>
-    pipe(
-      Future.tryCatch<unknown>(() => xml2js.parseStringPromise(xml, options)),
-      Future.map(flow(JSON.stringify, JSON.parse)),
-      Future.chainEitherK(u =>
-        pipe(decoder.decode(u), Either.mapLeft(decodeError(decoderName)(u))),
+function parseRawChanges(
+  changes: string,
+): Try<List<Tuple3<ChampionEnglishName, SpellName, string>>> {
+  return pipe(
+    changes,
+    string.split('\n'),
+    NonEmptyArray.init,
+    List.mkString('\n'),
+    string.split('\n|'),
+    NonEmptyArray.tail,
+    List.filterMap(
+      flow(
+        StringUtils.matcher3(nameSpellValueRegex),
+        Maybe.map(tuple =>
+          pipe(
+            tupleChampionEnglishNameSpellNameStringDecoder.decode(tuple),
+            Either.mapLeft(decodeError('Tuple3<ChampionEnglishName, SpellName, string>')(tuple)),
+          ),
+        ),
       ),
+    ),
+    List.sequence(Either.Applicative),
+  )
+}
+
+function makeTemplate(
+  grouped: ReadonlyMap<ChampionEnglishName, PartialDict<SpellName, string>>,
+): string {
+  return pipe(
+    grouped,
+    readonlyMap.toReadonlyArray(ChampionEnglishName.Ord),
+    List.map(([englishName, spells]) =>
+      StringUtils.stripMargins(
+        `== ${englishName} ==
+        |${pipe(
+          SpellName.values,
+          List.filterMap(spell =>
+            pipe(
+              Maybe.fromNullable(spells[spell]),
+              Maybe.map(changes =>
+                StringUtils.stripMargins(
+                  `=== ${spell} ===
+                  |{{ai|${spell}|${englishName}}}
+                  |${changes}`,
+                ),
+              ),
+            ),
+          ),
+          List.mkString('\n'),
+        )}`,
+      ),
+    ),
+    List.mkString('\n\n'),
+  )
+}
+
+function parseWikiHtml(html: string): IO<WikiaAramChanges> {
+  return pipe(
+    DomHandler.of()(html),
+    Either.map(domHandler => domHandler.window.document.body),
+    IO.fromEither,
+    IO.chainFirst(preProcessHtml),
+    IO.chainEitherK(
+      flow(
+        DomHandler.querySelectorAllNonEmpty('div.mw-parser-output > #toc ~ *'),
+        Either.mapLeft(
+          flow(
+            List.map(e => `- ${e}`),
+            List.mkString('\n'),
+            Error,
+          ),
+        ),
+      ),
+    ),
+    IO.chainEitherK(
+      flow(
+        parseGroupHtml,
+        Either.mapLeft(es => Error(es.join('\n'))),
+      ),
+    ),
+  )
+}
+
+function preProcessHtml(body: HTMLElement): IO<void> {
+  return () => {
+    /* eslint-disable functional/no-expression-statements */
+    body.querySelectorAll('script').forEach(e => e.remove())
+
+    body.querySelectorAll('img').forEach(e => {
+      const newUrl = pipe(
+        e.src,
+        StringUtils.matcher2(imageRegexWithSize),
+        Maybe.map(([a, b]) => `${a}/40px${b}`),
+        Maybe.alt(() => pipe(e.src, StringUtils.matcher1(imageRegex))),
+        Maybe.getOrElse(() => e.src),
+      )
+
+      e.setAttribute('src', new URL(newUrl, lolWikiDomain).toString())
+      e.removeAttribute('srcset')
+    })
+
+    body.querySelectorAll('a').forEach(e => {
+      e.setAttribute('href', new URL(e.href, lolWikiDomain).toString())
+      e.setAttribute('target', '_blank')
+      e.setAttribute('rel', 'noreferrer')
+    })
+
+    body.querySelectorAll('.inline-image').forEach(e => e.removeAttribute('style'))
+    /* eslint-enable functional/no-expression-statements */
+
+    return Try.success(undefined)
+  }
+}
+
+const validation = ValidatedNea.getValidation<string>()
+
+function parseGroupHtml(elements: ReadonlyArray<Element>): ValidatedNea<string, WikiaAramChanges> {
+  return pipe(
+    elements,
+    splitMapArray(e => (e.tagName === 'H2' ? Maybe.some(e) : Maybe.none)),
+    ValidatedNea.fromOption(() => "Elements did't start with a H2"),
+    ValidatedNea.chain(
+      List.traverse(validation)(([h2, elts]) =>
+        pipe(
+          apply.sequenceT(validation)(
+            parseFirstChildTextContent([ChampionEnglishName.codec, 'ChampionEnglishName'])(h2),
+            parseH3s(elts),
+          ),
+        ),
+      ),
+    ),
+    ValidatedNea.map(
+      flow(
+        ListUtils.groupByAsMap(ChampionEnglishName.Eq)(identity),
+        readonlyMap.map(NonEmptyArray.head),
+      ),
+    ),
+  )
+}
+
+function parseH3s(
+  elements: ReadonlyArray<Element>,
+): ValidatedNea<string, PartialDict<SpellName, ChampionSpellHtml>> {
+  return pipe(
+    elements,
+    splitMapArray(e => (e.tagName === 'H3' ? Maybe.some(e) : Maybe.none)),
+    ValidatedNea.fromOption(() => "Elements didn't start with a H3"),
+    ValidatedNea.chain(
+      List.traverse(validation)(([h3, elts]) =>
+        pipe(
+          apply.sequenceT(validation)(
+            parseFirstChildTextContent([SpellName.codec, 'SpellName'])(h3),
+            parseSpellHtml(elts),
+          ),
+        ),
+      ),
+    ),
+    ValidatedNea.map(
+      flow(
+        List.groupBy(([spell]) => spell),
+        PartialDict.map(flow(NonEmptyArray.head, Tuple.snd)),
+      ),
+    ),
+  )
+}
+
+const firstChildSelector = ':scope > :first-child'
+
+const parseFirstChildTextContent =
+  <A>([decoder, decoderName]: Tuple<Decoder<string, A>, string>) =>
+  (elt: Element): ValidatedNea<string, A> =>
+    pipe(
+      elt,
+      DomHandler.querySelectorEnsureOne(firstChildSelector),
+      Either.chain(DomHandler.textContent(firstChildSelector)),
+      Either.chain(text =>
+        pipe(decoder.decode(text), Either.mapLeft(decodeErrorString(decoderName)(text))),
+      ),
+      Either.mapLeft(NonEmptyArray.of),
     )
+
+function parseSpellHtml(elts: ReadonlyArray<Element>): ValidatedNea<string, ChampionSpellHtml> {
+  const [e1, e2, ...tail] = elts
+
+  if (e1 === undefined || e2 === undefined) {
+    return ValidatedNea.invalid(NonEmptyArray.of('Spell elements should be length 2 or more'))
+  }
+
+  return ValidatedNea.valid({
+    spell: e1.outerHTML, // TODO: take first child?
+    description: [e2, ...tail].map(e => e.outerHTML).join('\n'),
+  })
+}
 
 // Akshan Q = \n* Scoundrel duration reduced to 25 seconds.
 const nameSpellValueRegex = RegExp(
@@ -179,7 +306,8 @@ const nameSpellValueRegex = RegExp(
   's',
 )
 
-// https://static.wikia.nocookie.net/leagueoflegends/images/e/e9/Akshan_Going_Rogue.png/revision/latest/scale-to-width-down/20?cb=20210827174804
+// /en-us/images/thumb/Akshan_Going_Rogue.png/20px-Akshan_Going_Rogue.png?0893d
+const imageRegexWithSize = /^(.*\.png)\/\d+px(.*\.png).*$/
 const imageRegex = /^(.*\.png).*$/
 
 const parseParseTreeDecoder = D.struct({
@@ -213,3 +341,51 @@ const parseTextDecoder = D.struct({
     }),
   }),
 })
+
+const parseXML =
+  <A>(decoder: Decoder<unknown, A>, decoderName: string, options?: xml2js.ParserOptions) =>
+  (xml: string): Future<A> =>
+    pipe(
+      Future.tryCatch<unknown>(() => xml2js.parseStringPromise(xml, options)),
+      Future.map(flow(JSON.stringify, JSON.parse)),
+      Future.chainEitherK(u =>
+        pipe(decoder.decode(u), Either.mapLeft(decodeError(decoderName)(u))),
+      ),
+    )
+
+export const splitMapArray =
+  <A, B>(f: (a: A) => Maybe<B>) =>
+  (as: ReadonlyArray<A>): Maybe<ReadonlyArray<Tuple<B, ReadonlyArray<A>>>> => {
+    if (!List.isNonEmpty(as)) return Maybe.some([])
+
+    const [head, ...tail] = as
+
+    const init = f(head)
+
+    if (Maybe.isNone(init)) return Maybe.none
+
+    /* eslint-disable functional/no-let */
+    let b: B = init.value
+    let acc: A[] = []
+    /* eslint-enable functional/no-let */
+
+    const res: Tuple<B, ReadonlyArray<A>>[] = []
+
+    /* eslint-disable functional/no-expression-statements, functional/immutable-data*/
+    tail.forEach(a => {
+      const opt = f(a)
+
+      if (Maybe.isSome(opt)) {
+        res.push([b, acc])
+        b = opt.value
+        acc = []
+      } else {
+        acc.push(a)
+      }
+    })
+
+    res.push([b, acc])
+    /* eslint-enable functional/no-expression-statements, functional/immutable-data */
+
+    return Maybe.some(res)
+  }
---
+++ test/server/services/staticDataService/getFetchWikiaAramChanges.spec.ts
@@ -0,0 +1,81 @@
+import { pipe } from 'fp-ts/function'
+
+import { Either, Maybe } from '../../../../src/shared/utils/fp'
+
+import { splitMapArray } from '../../../../src/server/services/staticDataService/getFetchWikiaAramChanges'
+
+import { expectT } from '../../../expectT'
+
+describe('getFetchWikiaAramChanges', () => {
+  describe('splitMapArray', () => {
+    it('should split map empty array', () => {
+      const actual = pipe(
+        [],
+        splitMapArray(() => Maybe.none),
+      )
+
+      expectT(actual).toStrictEqual(Maybe.some([]))
+    })
+
+    it('should split map base case', () => {
+      const actual = pipe(
+        [
+          Either.left(1),
+          Either.right('a'),
+          Either.right('b'),
+          Either.left(2),
+          Either.right('c'),
+          Either.right('d'),
+        ],
+        splitMapArray(e => (Either.isLeft(e) ? Maybe.some(e.left) : Maybe.none)),
+      )
+
+      expectT(actual).toStrictEqual(
+        Maybe.some([
+          [1, [Either.right('a'), Either.right('b')]],
+          [2, [Either.right('c'), Either.right('d')]],
+        ]),
+      )
+    })
+
+    it('should fail for none for first case', () => {
+      const actual = pipe(
+        [
+          Either.right('a'),
+          Either.right('b'),
+          Either.left(2),
+          Either.right('c'),
+          Either.right('d'),
+        ],
+        splitMapArray(e => (Either.isLeft(e) ? Maybe.some(e.left) : Maybe.none)),
+      )
+
+      expectT(actual).toStrictEqual(Maybe.none)
+    })
+
+    it('should handle more complex case', () => {
+      const actual = pipe(
+        [
+          Either.left(1),
+          Either.left(2),
+          Either.right('a'),
+          Either.right('b'),
+          Either.left(3),
+          Either.right('c'),
+          Either.right('d'),
+          Either.left(4),
+        ],
+        splitMapArray(e => (Either.isLeft(e) ? Maybe.some(e.left) : Maybe.none)),
+      )
+
+      expectT(actual).toStrictEqual(
+        Maybe.some([
+          [1, []],
+          [2, [Either.right('a'), Either.right('b')]],
+          [3, [Either.right('c'), Either.right('d')]],
+          [4, []],
+        ]),
+      )
+    })
+  })
+})
