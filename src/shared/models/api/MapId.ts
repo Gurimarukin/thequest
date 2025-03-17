8b6b4541643410af38aabd35acd69a39463828e3
Gurimarukin <naxel@laposte.net>
2025-03-17 19:00:53 +0100

    remove useless howlingAbyssMaps

--- src/shared/models/api/MapId.ts
+++ src/shared/models/api/MapId.ts
@@ -11,20 +11,14 @@ const summonersRiftMaps = [
   11, // Summoner's Rift — Current Version
 ] as const
 
-type HowlingAbyssMap = (typeof howlingAbyssMaps)[number]
-
-const howlingAbyssMaps = [
-  12, // Howling Abyss — ARAM map
-  14, // Butcher's Bridge — Alternate ARAM map
-] as const
-
 const e = createEnum(
   ...summonersRiftMaps,
-  ...howlingAbyssMaps,
   3, // The Proving Grounds — Tutorial Map
   4, // Twisted Treeline — Original Version
   8, // The Crystal Scar — Dominion map
   10, // Twisted Treeline — Last TT map
+  12, // Howling Abyss — ARAM map
+  14, // Butcher's Bridge — Alternate ARAM map
   16, // Cosmic Ruins — Dark Star: Singularity map
   18, // Valoran City Park — Star Guardian Invasion map
   19, // Substructure 43 — PROJECT: Hunters map
@@ -37,9 +31,6 @@ const e = createEnum(
 const isSummonersRift = (map: MapId): map is SummonersRiftMap =>
   List.elem(e.Eq)(map, summonersRiftMaps)
 
-const isHowlingAbyss = (map: MapId): map is HowlingAbyssMap =>
-  List.elem(e.Eq)(map, howlingAbyssMaps)
-
-const MapId = { ...e, isSummonersRift, isHowlingAbyss }
+const MapId = { ...e, isSummonersRift }
 
 export { MapId }
